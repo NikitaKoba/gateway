6e6cc45bbb90bce86ace769fdcc3037e5c23f103
Nikita Koba <Mactavish1811@gmail.com>
2023-05-19 15:41:47 +0400

    test

---
+++ src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -0,0 +1,9 @@
+package com.example.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+}
